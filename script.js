0842edabd6390e4cacff3c57a72f6525b4170692
Valentina Corak <valentinacorak@gmail.com>
2021-05-06 15:04:31 +0200

    Finnaly can search with input id and name togethergit add script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const input = +document.getElementById('input-id') || document.getElementById('input-id');
+const button = document.getElementById('button-addon1');
 
+const getPokemon = async term => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${term}`
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    console.log(pokemon);
+}
+
+
+button.addEventListener('click', () => getPokemon(input.value));
 
 
