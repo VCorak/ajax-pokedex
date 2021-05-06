127ed12b28402d0cc8e149eb7eb1573be36e6429
Valentina Corak <valentinacorak@gmail.com>
2021-05-06 12:03:23 +0200

    Second try to fetch pokemon, works in console...

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+const getRandomPoke = (min, max) => {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
 
+console.log(getRandomPoke(1, 1118))
 
 function getInputValue() {
     document.getElementById("button-addon1").addEventListener('click', () => {
         let input = +document.getElementById("input-id").value;
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                
-            })
-    })
-}
+        fetchPokemon();
+    })}
+
+        const fetchPokemon = async () => {
+            try {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon/150')
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+getInputValue();
+
