93fe0c2990d40412b35648a145e4be408c9b6958
Valentina Corak <valentinacorak@gmail.com>
2021-05-06 14:34:50 +0200

    Deleted fetch, going from beggining...

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const getRandomPoke = (min, max) => {
-    return Math.floor(Math.random() * (max-min)) + min;
-}
 
-console.log(getRandomPoke(1, 1118))
-
-function getInputValue() {
-    document.getElementById("button-addon1").addEventListener('click', () => {
-        let input = +document.getElementById("input-id").value || document.getElementById("input-id").value;
-        fetchPokemon();
-        console.log(input);
-    })}
-getInputValue();
-
-        const fetchPokemon = async () => {
-            try {
-                const response = await fetch('https://pokeapi.co/api/v2/pokemon/150')
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
 
 
