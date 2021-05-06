7ca51d4b5eafcfe53608035453a04405f662c833
Valentina Corak <valentinacorak@gmail.com>
2021-05-06 18:25:04 +0200

    No sucess yet to adding moves...

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const input = +document.getElementById('input-id') || document.getElementById('input-id');
+const input = document.getElementById('input-id');
 const button = document.getElementById('button-addon1');
 
 const getPokemon = async term => {
     const url = `https://pokeapi.co/api/v2/pokemon/${term}`
     const response = await fetch(url);
+
+
     const pokemon = await response.json();
+
     console.log(pokemon);
+
+    document.getElementById('image').setAttribute('src', pokemon.sprites.other.dream_world.front_default )
+    document.getElementById('pokemon_name').innerHTML = pokemon.name.toUpperCase();
+    document.getElementById('pokemon_id').innerHTML = `ID: ${pokemon.id}`;
+    document.getElementById('pokemon_moves').innerHTML = "";
+    document.getElementById('pokemon_type').innerHTML = 'TYPE: ' + pokemon.types.map((type) => type.type.name).join(', ');
 }
 
-
 button.addEventListener('click', () => getPokemon(input.value));
-
-
